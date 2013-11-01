aedc624fc792f0990061fd92d4c183e3a30fdd17
Craig Spaeth <craigspaeth@gmail.com>
2013-11-01 17:58:58 -0400

    alert errors and better handle coffeescript

--- index.js
+++ index.js
@@ -9,14 +9,15 @@ module.exports = function(options) {
       var b = browserify()
         , path = options.src + req.url;
       if (!exists(path))
-        path = options.src + '/' + basename(req.url, '.js') + '.coffee';   
+        path = options.src + req.url.replace('.js', '') + '.coffee';   
       if (exists(path)) b.add(path);
       (options.transforms || []).forEach(function(t) {
+        console.log(t);
         b.transform(t);
       });
       b.bundle(function(err, text) {
         if (err) {
-          res.send("throw 'BROWSERIFY COMPILE ERROR: " + err.toString() + "')");
+          res.send("alert(\"BROWSERIFY COMPILE ERROR: " + err.message + "\");");
         } else {
           res.send(text);
         }
